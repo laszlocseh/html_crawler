46b3231a96f97c269ae35dd69d890bace5c93fd7
laszlocseh <csehlaszlo91@gmail.com>
2018-02-12 22:20:59 +0200

    some modifications

--- https_crawler/spiders/climate_laci.py
+++ https_crawler/spiders/climate_laci.py
@@ -14,17 +14,22 @@ class ClimateLaciSpider(scrapy.Spider):
     def parse(self, response):
         try:
             pages = response.xpath('//@href[substring(., 1, 5) = "http:"]')
-        except NotSupported:
-            pages = None
+        except Exception:
+            pages = ()
         for href in pages:
             url = href.extract()
             url_parsed = urlparse(url)
             if url not in self.visited:
                 self.visited.add(url)
                 if url_parsed.netloc not in self.allowed_domains:
-                    yield {
-                        'url': url,
-                        'url_page_source': response.url
-                    }
+                    url_https = url.replace('http:', 'https:')
+                    request_status_code = requests.get(url_https).status_code
+                    if request_status_code != 200:
+                        yield {
+                            'url': url,
+                            'url_https': url_https,
+                            'status_code': request_status_code,
+                            'url_page_source': response.url
+                        }
                 if url_parsed.netloc in self.allowed_domains:
                     yield response.follow(href, callback=self.parse)
