69908979ca9825807ba152e0302cc2e35405ba39
laszlocseh <csehlaszlo91@gmail.com>
2018-02-15 14:53:17 +0200

    updated iframe, script crawler

--- https_crawler/spiders/climate_iframe.py
+++ https_crawler/spiders/climate_iframe.py
@@ -9,30 +9,54 @@ class ClimateLaciSpider(scrapy.Spider):
     name = 'climate-iframe'
     allowed_domains = ['climate-adapt.eea.europa.eu']
     start_urls = ['http://climate-adapt.eea.europa.eu']
+
     visited = set()
+    iframe_checked = dict()
+
+    def get_url_complete(self, page, url):
+        page = page.strip()
+        if not page.startswith('http'):
+            url_complete = urljoin(url, page)
+        else:
+            url_complete = page
+        return url_complete
 
     def parse(self, response):
         try:
-            iframes = response.xpath('//iframe/@src').extract()
+            iframes = set(response.xpath('//iframe/@src').extract())
         except NotSupported:
             iframes = set()
 
         for iframe_url in iframes:
-            iframe_url_https = iframe_url.strip().replace('http:', 'https:')
-            request_status_code = requests.get(iframe_url_https).status_code
-            yield {
-                'iframe': iframe_url,
-                'iframe_https': iframe_url_https,
-                'status_code': request_status_code,
-                'url_page_source': response.url
-            }
+            iframe_url = self.get_url_complete(iframe_url, response.url)
+            iframe_url_https = iframe_url.replace('http:', 'https:')
+            iframe_url_parsed = urlparse(iframe_url)
+            if iframe_url_parsed.netloc not in self.allowed_domains:
+                if iframe_url_parsed not in self.iframe_checked:
+                    try:
+                        request_status_code = requests.get(iframe_url).status_code
+                    except ConnectionError:
+                        request_status_code = 0
+                    self.iframe_checked.update({iframe_url: request_status_code})
+                else:
+                    request_status_code = self.iframe_checked.get(iframe_url, 0)
+                if request_status_code:
+                    yield {
+                        'iframe': iframe_url,
+                        'iframe_https': iframe_url_https,
+                        'status_code': request_status_code,
+                        'url_page_source': response.url
+                    }
         try:
-            next_pages = response.xpath('//a/@href[substring(., 1, 1) = "/" or substring(., 1, 1) = "."]').extract()
+            # next_pages = response.xpath('//a/@href[substring(., 1, 1) = "/" or substring(., 1, 1) = "."]').extract()
+            next_pages = response.xpath(
+                '//a[not(contains(@href, "@")) and not(contains(@href, "more-events"))]/@href').extract()
+            # next_pages = response.xpath('//a/@href').extract()
+            next_pages = set(next_pages) - self.visited
         except NotSupported:
             next_pages = set()
         for page in next_pages:
-            url_complete = urljoin(response.url, page)
+            url_complete = self.get_url_complete(page, response.url)
             if url_complete not in self.visited:
                 self.visited.add(url_complete)
                 yield response.follow(page, callback=self.parse)
-
--- https_crawler/spiders/climate_script.py
+++ https_crawler/spiders/climate_script.py
@@ -10,24 +10,37 @@ class ClimateLaciSpider(scrapy.Spider):
     name = 'climate-script'
     allowed_domains = ['climate-adapt.eea.europa.eu']
     start_urls = ['http://climate-adapt.eea.europa.eu']
+
     visited = set()
-    script_checked = set()
+    script_checked = dict()
+
+    def get_url_complete(self, page, url):
+        page = page.strip()
+        if not page.startswith('http'):
+            url_complete = urljoin(url, page)
+        else:
+            url_complete = page
+        return url_complete
 
     def parse(self, response):
         try:
-            scripts = response.xpath('//script/@src').extract()
+            scripts = set(response.xpath('//script/@src').extract())
         except NotSupported:
             scripts = set()
 
         for script_url in scripts:
-            script_url = response.urljoin(script_url.strip())
+            script_url = self.get_url_complete(script_url, response.url)
             script_url_https = script_url.replace('http:', 'https:')
-            if script_url_https not in self.script_checked:
-                self.script_checked.add(script_url_https)
-                try:
-                    request_status_code = requests.get(script_url_https).status_code
-                except ConnectionError:
-                    request_status_code = 0
+            script_url_parsed = urlparse(script_url)
+            if script_url_parsed.netloc not in self.allowed_domains:
+                if script_url_parsed not in self.script_checked:
+                    try:
+                        request_status_code = requests.get(script_url).status_code
+                    except ConnectionError:
+                        request_status_code = 0
+                    self.script_checked.update({script_url: request_status_code})
+                else:
+                    request_status_code = self.script_checked.get(script_url, 0)
                 if request_status_code != 200:
                     yield {
                         'script': script_url,
@@ -36,11 +49,15 @@ class ClimateLaciSpider(scrapy.Spider):
                         'url_page_source': response.url
                     }
         try:
-            next_pages = response.xpath('//a/@href[substring(., 1, 1) = "/" or substring(., 1, 1) = "."]').extract()
+            # next_pages = response.xpath('//a/@href[substring(., 1, 1) = "/" or substring(., 1, 1) = "."]').extract()
+            next_pages = response.xpath(
+                '//a[not(contains(@href, "@")) and not(contains(@href, "more-events"))]/@href').extract()
+            # next_pages = response.xpath('//a/@href').extract()
+            next_pages = set(next_pages) - self.visited
         except NotSupported:
             next_pages = set()
         for page in next_pages:
-            url_complete = urljoin(response.url, page)
+            url_complete = self.get_url_complete(page, response.url)
             if url_complete not in self.visited:
                 self.visited.add(url_complete)
                 yield response.follow(page, callback=self.parse)
